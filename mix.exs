463e53da9b18125be3de0969f5671d64a8324b62
Grant Nelson <grantmnelsn@gmail.com>
2025-01-20 18:45:17 -0500

    using known-good version from another app deployed on H

--- mix.exs
+++ mix.exs
@@ -5,8 +5,9 @@ defmodule Charityapidocs.MixProject do
     [
       app: :charityapidocs,
       version: "1.0.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env == :prod,
       deps: deps(),
       aliases: aliases(),
 
